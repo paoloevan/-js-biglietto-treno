6e4455225862715415e0ff0f3d7d911f44f67e42
paoloevan <paoloev1900@gmail.com>
2022-10-06 15:01:13 +0200

    add km and age prompt

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+/*Il programma dovrà chiedere all'utente:
+ il numero di chilometri che vuole percorrere
+ e l'età del passeggero.
+ Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+ il prezzo del biglietto è definito in base ai km (0.21 € al km)
+ va applicato uno sconto del 20% per i minorenni
+ va applicato uno sconto del 40% per gli over 65.*/
+
+ //chiedere numero chilometri da percorrere
+ //chiedere età
+
+ const kmRequest = prompt("Quanti chilometri devi percorrere?");
+ const userAge = prompt("Quanti anni hai?");
+ console.log(kmRequest);
+ console.log(userAge);
