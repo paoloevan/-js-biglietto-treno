4fc02c650adf40ddb801678eab598b5b3d5182b1
paoloevan <paoloev1900@gmail.com>
2022-10-06 15:07:54 +0200

    add price calculation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,21 @@
 /*Il programma dovrà chiedere all'utente:
- il numero di chilometri che vuole percorrere
- e l'età del passeggero.
- Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
- il prezzo del biglietto è definito in base ai km (0.21 € al km)
- va applicato uno sconto del 20% per i minorenni
- va applicato uno sconto del 40% per gli over 65.*/
+il numero di chilometri che vuole percorrere
+e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.*/
 
- //chiedere numero chilometri da percorrere
- //chiedere età
+//chiedere numero chilometri da percorrere
+//chiedere età
+const kmRequest = prompt("Quanti chilometri devi percorrere?");
+const userAge = prompt("Quanti anni hai?");
+console.log(kmRequest);
+console.log(userAge);
 
- const kmRequest = prompt("Quanti chilometri devi percorrere?");
- const userAge = prompt("Quanti anni hai?");
- console.log(kmRequest);
- console.log(userAge);
+//calcolo prezzo biglietto in base ai chilometri
+let userPrice = kmRequest * 0.21;
+console.log("il prezzo in base ai chilometri è", userPrice);
+
+//sconto 20% minorenni e sconto 40% over 65
+if 
